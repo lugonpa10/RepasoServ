cdac2c6359f417dcf67443b91d884db8ea403721
lugonpa10 <lugonpa10@gmail.com>
2026-05-26 21:02:58 +0200

    Ejercicio 3.2

---
+++ Ejercicio 3.2/ConsoleApp1/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static int cont = 0;
+        static bool flag = true;
+        static readonly object l = new object();
+        static void Main(string[] args)
+        {
+            Thread th1 = new Thread(() =>
+            {
+
+                while (flag)
+                {
+
+                    lock (l)
+                    {
+                        if (!flag)
+                        {
+                            break;
+                        }
+                        cont++;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write($"{cont}\t");
+                        Console.ResetColor();
+
+                        if (cont >= 500)
+                        {
+                            flag = false;
+                            break;
+                        }
+                    }
+                }
+            });
+
+            Thread th2 = new Thread(() =>
+            {
+                while (flag)
+                {
+                    lock (l)
+                    {
+                        if (!flag)
+                        {
+                            break;
+                        }
+                        cont--;
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write($"{cont}\t");
+                        Console.ResetColor();
+                        if (cont <= -500)
+                        {
+                            flag = false;
+                            break;
+                        }
+
+
+                    }
+
+
+                }
+            });
+            th1.Start();
+            th2.Start();
+            th1.Join();
+            th2.Join();
+            if (cont >= 500)
+            {
+                Console.WriteLine("Ganador hilo 1");
+            }
+            else if (cont <= -500)
+            {
+                Console.WriteLine("Ganador hilo 2");
+            }
+        }
+    }
+}
---
+++ Ejercicio 3.2/Ejercicio 3.2/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace Ejercicio_3._2
+{
+    internal class Program
+    {
+        static bool running = true;
+        static readonly object l = new();
+        static int i = 0;
+        static int ganador;
+        static void Main(string[] args)
+        {
+            Thread hiloIncrementar = new Thread(incrementar);
+            Thread hiloDecrementar = new Thread(decrementar);
+            hiloIncrementar.Start();
+            hiloDecrementar.Start();
+            hiloIncrementar.Join();
+            hiloDecrementar.Join();
+            if (ganador == 1)
+            {
+                Console.WriteLine("\nEl ganador es incrementar");
+
+            }
+            else
+            {
+                Console.WriteLine("\nEl ganador es incrementar");
+
+            }
+        }
+
+        static void incrementar()
+        {
+            while (running)
+            {
+                lock (l)
+                {
+                    i++;
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write($"{i}\t");
+                    Console.ResetColor();
+
+
+                    if (i >= 500)
+                    {
+                        running = false;
+                        ganador = 1;
+
+                    }
+                }
+
+            }
+
+        }
+
+        static void decrementar()
+        {
+
+            while (running)
+            {
+                lock (l)
+                {
+                    i--;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write($"{i}\t");
+                    Console.ResetColor();
+                    if (i <= -500)
+                    {
+                        running = false;
+                        ganador = 2;
+                    }
+                }
+            }
+        }
+
+
+    }
+}
